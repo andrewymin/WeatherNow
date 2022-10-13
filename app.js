bdadecfbbebe41d35bc7c9b915059622536ae6a6
Andy M <yoongi9289@gmail.com>
2022-10-12 23:18:09 -0600

    Change fetch and rebuild for server

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cors());
 // app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-let port = process.env.port || 8000;
+let port = process.env.port || 3001;
 // var apiKey = process.env.weatherKey;
 
 app.get('/', (req, res)=> {
